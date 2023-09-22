610ef9e2c42e5605fff9921f6dd2e27909b2f28c
Layla Hammoud <laylahammoud63@gmail.com>
2023-09-22 10:43:39 +0300

    step 3

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const currentDate = new Date();
+
+app.get('/test',(request,response)=>{
+    response.send({status:200, message:"ok"})
+})
+
+app.get('/time',(request,response)=>{
+    response.send({status:200, message:`${currentDate.getHours()}:${currentDate.getMinutes()}`})
+})
+
 app.get('/',(request,response)=>{
     response.send('ok')
 })
