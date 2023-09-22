b934f24b9b10860e57f1593305b30edf8c1dfa14
Layla Hammoud <laylahammoud63@gmail.com>
2023-09-22 15:27:34 +0300

    step 4

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const currentDate = new Date();
 
-app.get('/test',(request,response)=>{
-    response.send({status:200, message:"ok"})
-})
+app.get("/test", (request, response) => {
+  response.send({ status: 200, message: "ok" });
+});
 
-app.get('/time',(request,response)=>{
-    response.send({status:200, message:`${currentDate.getHours()}:${currentDate.getMinutes()}`})
-})
+app.get("/time", (request, response) => {
+  response.send({
+    status: 200,
+    message: `${currentDate.getHours()}:${currentDate.getMinutes()}`,
+  });
+});
 
-app.get('/',(request,response)=>{
-    response.send('ok')
-})
+app.get("/", (request, response) => {
+  response.json("ok");
+});
 
-app.listen(3000,(request,response)=>{
-    console.log(`the server is running on port 3000`)
-})
+app.get("/hello/:id?", (request, response) => {
+  response.json(request.params.id ? `Hello ${request.params.id}!` : `Hello!`);
+});
+
+app.get("/search", (request, response) => {
+  let searchData = request.query.s;
+  if(searchData){
+    response.json({status:200, message:"ok", data:searchData});
+  }else{
+    response.status(500).json({status:500, error:true, message:"you have to provide a search"})
+  }
+});
+
+app.listen(3000, (request, response) => {
+  console.log(`the server is running on port 3000`);
+});
